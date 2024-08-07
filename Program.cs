32a12efa7681c444e62493d94e5586c01c0fd63d
leminhtri1511 <trile20021511@gmail.com>
2024-08-07 20:46:04 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TinhChiSoCanNang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height;
+            double weight;
+
+            Console.WriteLine("Please enter a height");
+            height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your weight");
+            weight = float.Parse(Console.ReadLine());
+
+            double bmi = weight / Math.Pow(height, 2);
+            bmi = Math.Round(bmi, 1);
+
+            if (bmi < 18)
+                Console.WriteLine(" Underweight");
+            else if (bmi < 25.0)
+                Console.WriteLine(" Normal");
+            else if (bmi < 30.0)
+                Console.WriteLine(" Overweight");
+            else
+                Console.WriteLine(" Obese");
+                
+            Console.Write("BMI: " + bmi);
+        }
+    }
+}
